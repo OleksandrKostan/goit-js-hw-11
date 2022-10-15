2dbadad2c6a1fc12f7732f0dd6db3d7c9f529751
Oleksandr Kostan <kostan.law@gmail.com>
2022-10-15 14:47:51 +0300

    fixst

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { PixabayAPI } from './pixabay-api';
+import { PixabayAPI } from './js/pixabay-api';
 import galleryCardsTemplate from '../templates/gallery-card.hbs';
 import Notiflix from 'notiflix';
 
